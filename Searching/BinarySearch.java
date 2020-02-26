c84b7739d873a40b4937f5fff37c1b9303143abb
swetajain23 <49022680+swetajain23@users.noreply.github.com>
2020-02-26 09:52:29 +0530

    Add files via upload

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.impetus.searching;
+
+public class BinarySearch {
+
+    static int search(int []a, int low, int high , int ele){
+        int mid = (low+high)/2;
+        if(low>high)
+            return -1;
+        if(a[mid] == ele)
+            return mid;
+        if(a[mid]>ele)
+            return search(a,low,mid-1,ele);
+        else
+            return search(a, mid, high, ele);
+    }
+
+    public static void main(String [] args){
+        int a[] = {2,4,6,8,9,12,15,17};
+        int n = a.length;
+        int index = search(a, 0, n , 15);
+        System.out.println("index " +index);
+    }
+}
+
+
